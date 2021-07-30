4ecbd7f1f372990b1c594fdeb56cbf30cd88b8db
hkbhaskar <67137854+hkbhaskar@users.noreply.github.com>
2021-07-30 11:44:50 +0530

    Initial commit

---
+++ wp-content/themes/hudson/js/default_dynamic.js
@@ -0,0 +1,50 @@
+
+        var header_height = 87;
+        var min_header_height_scroll = 57;
+        var min_header_height_sticky = 60;
+        var scroll_amount_for_sticky = 85;
+        var min_header_height_fixed_hidden = 45;
+        var header_bottom_border_weight = 1;
+        var scroll_amount_for_fixed_hiding = 200;
+        var menu_item_margin = 0;
+        var large_menu_item_border = 0;
+        var element_appear_amount = -150;
+        var paspartu_width_init = 0.02;
+        var directionNavArrows = 'arrow_carrot-';
+        var directionNavArrowsTestimonials = 'fa fa-angle-';
+        var enable_navigation_on_full_screen_section = false;
+        var add_for_admin_bar = jQuery('body').hasClass('admin-bar') ? 32 : 0;
+
+                                        
+        
+        
+        
+        
+        var logo_height = 130; // hudson logo height
+        var logo_width = 280; // hudson logo width
+                logo_width = 720;
+        logo_height = 304;
+
+        
+                        header_top_height = 0;
+                    var loading_text;
+        loading_text = 'Loading new posts...';
+        var finished_text;
+        finished_text = 'No more posts';
+
+        var piechartcolor;
+        piechartcolor	= "#b48360";
+
+        
+        
+        
+        
+
+        var no_ajax_pages = [];
+        var edgt_root = 'https://www.loanbhumi.in/';
+        var theme_root = 'https://www.loanbhumi.in/wp-content/themes/hudson/';
+                    var header_style_admin = "";
+                if(typeof no_ajax_obj !== 'undefined') {
+        no_ajax_pages = no_ajax_obj.no_ajax_pages;
+        }
+        
